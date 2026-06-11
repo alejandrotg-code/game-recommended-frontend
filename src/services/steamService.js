36deb8af95fcb0fd19fe6f00324df68b7a5fbf41
Alejandro Tacoronte González <alejandrotacorontegnlez@gmail.com>
2026-06-11 12:33:01 +0100

    Feat/badge and details (#16)
    * feat: show game details and add markdown badge copy option

--- src/services/steamService.js
+++ src/services/steamService.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL;
+const API_BASE_URL = import.meta.env.VITE_API_URL_DEV;
 
 /**
  * Busca juegos en Steam que coincidan con el término proporcionado.
